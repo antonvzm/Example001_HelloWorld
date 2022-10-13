37c6cb6f946505c4f0f5ce9ed320f9632088cc02
Anton <antonvzm@gmail.com>
2022-10-13 23:13:24 +0300

    add Example005

--- Example005_ConditiomIfElse/Program.cs
+++ Example005_ConditiomIfElse/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите имя пользователя:");
+ string username = Console.ReadLine();
+
+if (username.ToLower() == "маша") 
+{
+Console.WriteLine("Ура, это же Маша!!!");
+}
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine(username);
+}
